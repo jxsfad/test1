09dbbbf93eaef28081b7c1010c262aef488e4960
jxsfad <jxsfad@163.com>
2018-06-04 15:22:26 +0800

    first 2 fiels

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+#-*- coding:utf-8 -*-
+#Author:demo
+#Email:demo@demo.com
+#Version:demo
+import tornado.ioloop
+import tornado.web
+import os
+import sys
+from tornado.options import define,options
+from common.url_router import include,url_wrapper
+
+class Application(tornado.web.Application):
+	def __init__(self):
+		handlers = url_wrapper([
+        (r"/users/", include('views.users.users_urls'))
+        ])
+		#定义Tornado 服务器的配置项，如static/template目录的位置,debug级别等
+		settings=dict(
+		debug=True,
+		static_path=os.path.join(os.path.dirname(__file__),"static"),
+		template_path=os.path.join(os.path.dirname(__file__),"templates")
+		)
+		tornado.web.Application.__init__(self,handlers,**settings)
+
+
+if __name__ =='__main__':
+	print("Tornado server is ready for service\r")
+	tornado.options.parse_command_line()
+	Application().listen(8000,xheaders=True)
+	tornado.ioloop.IOLoop.instance().start()
+	
